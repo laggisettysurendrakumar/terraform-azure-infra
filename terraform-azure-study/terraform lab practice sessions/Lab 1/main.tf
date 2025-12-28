e9a5a0d60223b2f8e3e668147ae7d26a324a519f
LaggisettySurendraKumar <surilsk4@gmail.com>
2025-12-28 17:22:10 +0530

    Create main.tf

---
+++ terraform-azure-study/terraform lab practice sessions/Lab 1/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "XYZCompany/azurerm"
+      version = "2.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "training_vnet" {
+  name                = "xyz-vnet-eastus"
+  location            = "East US"
+  resource_group_name = "rg-xyz-training"
+  address_space       = ["10.20.0.0/16"]
+}
