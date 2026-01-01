4a2cfe3bf7fd6d1eb083e59a98b12439260098c1
CodeAdmirer <surilsk4@gmail.com>
2026-01-01 18:30:42 +0530

    Added lab practice for Lab 11

---
+++ terraform-azure-study/terraform lab practice sessions/Lab 11/terraformlab/main.tf
@@ -0,0 +1,49 @@
+# Terraform
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.40.0"
+    }
+  }
+}
+
+#Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# NSG module
+module "nsg" {
+    count = 3 
+    source = "./modules/nsg"
+
+    nsgname    = "nsg"
+    rgname    = "cal-3323-715"
+    location  = "westus"
+
+    nsg_rule =[
+    {
+    name                       = "http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    },
+    {
+     name                       = "ssh"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "22"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    }
+]
+}
